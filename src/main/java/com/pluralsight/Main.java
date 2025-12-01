ac3d6a9d24169639b062c78bb023b297eb0ca6bf
arsenii <arseniy.kunilovskiy@gmail.com>
2025-12-01 13:50:27 -0500

    exercise is done

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -14,8 +14,7 @@ public class Main {
             dataSource.setUrl("jdbc:mysql://localhost:3306/sakila");
             dataSource.setUsername(args[0]);
             dataSource.setPassword(args[1]);
-            try (Connection connection = dataSource.getConnection();
-                Scanner scanner = new Scanner(System.in)) {
+            try (Scanner scanner = new Scanner(System.in)) {
 
                 if (args.length != 2) {
                     System.out.println("Application needs two arguments to run: " +
@@ -25,37 +24,71 @@ public class Main {
 
                 System.out.println("Hello! You are in the SakilaMovies archive.");
                 String lastname = "";
-                while (true){
+                String firstname = "";
+                while (true) {
                     System.out.println("Please enter a last name of an actor you like:");
                     lastname = scanner.nextLine();
-                    displayActorsByLastName(connection, lastname);
+                    displayActorsByLastName(dataSource, lastname);
                     System.out.println("Please specify the first name of an actor you like:");
+                    firstname = scanner.nextLine();
+                    displayMoviesByFullName(dataSource, firstname, lastname);
+
                 }
 
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static void displayActorsByLastName(Connection connection, String lastname){
-        try (PreparedStatement statement = connection.prepareStatement("Select first_name, last_name from actor " +
-                "where last_name = ?")){
+    public static void displayActorsByLastName(BasicDataSource dataSource, String lastname) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement("""
+                     Select first_name, last_name from actor
+                     where last_name = ?""")) {
             statement.setString(1, lastname);
-            try (ResultSet results = statement.executeQuery()){
-                if (!results.next()){
+            try (ResultSet results = statement.executeQuery()) {
+                if (!results.next()) {
                     System.out.println("There is no actor with the last name " + lastname);
                     return;
                 }
-               do {
-                    String firstName = results.getString(1);
-                    String lastName = results.getString(2);
+                String firstName;
+                String lastName;
+                do {
+                    firstName = results.getString(1);
+                    lastName = results.getString(2);
                     System.out.println(firstName + " " + lastName);
                 } while (results.next());
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
+    }
 
+    public static void displayMoviesByFullName(BasicDataSource dataSource, String firstName, String lastName) {
+        try (Connection connection = dataSource.getConnection();
+             PreparedStatement statement = connection.prepareStatement(""" 
+                     SELECT f.title FROM sakila.film f
+                     join film_actor fa on f.film_id = fa.film_id
+                     join actor a on fa.actor_id = a.actor_id
+                     where first_name = ? AND last_name = ?;""")) {
+            statement.setString(1, firstName);
+            statement.setString(2, lastName);
+            try (ResultSet results = statement.executeQuery()) {
+                if (!results.next()) {
+                    System.out.println("There is no movies with this actor");
+                    return;
+                }
+                String title;
+                System.out.println("Here is the list of movies with " + firstName + " " + lastName+ "-----------------");
+                do {
+                    title = results.getString(1);
+                    System.out.println(title);
+                } while (results.next());
+                System.out.println("End of the list--------------------------------------------");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
